9f486475c03b473f0ea4965e2a3764c85450fabd
Teresa Hu <teresa@teresahu.net>
2017-10-20 12:55:10 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var currValue;
+
+function storeValue(value) {
+  currValue = value;
+}
+
+function compareValue(value){
+  console.log("currValue "+currValue);
+  console.log("onfocusout "+value);
+  if (currValue != value) {
+    console.log("FLASH");
+  }
+}
