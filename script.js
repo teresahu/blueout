f5b0e41e5dc6721cce1ba0379def545a5b45c6d3
Teresa Hu <teresa@teresahu.net>
2017-10-20 16:04:52 -0700

    Adding nav menu

--- script.js
+++ script.js
@@ -11,3 +11,8 @@ function compareValue(value){
     console.log("FLASH");
   }
 }
+
+function toggleMenu() {
+  var bugMenu = document.getElementById("bug-menu");
+  bugMenu.style.visibility = (bugMenu.style.visibility==="visible") ? "hidden" : "visible";
+}
