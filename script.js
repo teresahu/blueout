0d11da19293fe6ba4049bf91b8167a984f508563
Teresa Hu <teresa@teresahu.net>
2017-10-21 15:52:50 -0700

    Added task inbox

--- script.js
+++ script.js
@@ -16,3 +16,10 @@ function toggleMenu() {
   var bugMenu = document.getElementById("bug-menu");
   bugMenu.style.visibility = (bugMenu.style.visibility==="visible") ? "hidden" : "visible";
 }
+
+function openTaskInbox() {
+  var taskInbox = document.getElementById("task-inbox");
+  if (taskInbox.style.visibility === "" || taskInbox.style.visibility === "hidden") {
+    taskInbox.style.visibility = "visible";
+  }
+}
