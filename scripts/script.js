99e8122ca29b41530e94fa322eb58c95fe7dc2b4
Teresa Hu <teresa@teresahu.net>
2017-11-01 14:02:49 -0700

    Added flash effect on value change

--- scripts/script.js
+++ scripts/script.js
@@ -5,11 +5,36 @@ function storeValue(value) {
   currValue = value;
 }
 
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+}
+
 function compareValue(value){
-  console.log("currValue "+currValue);
-  console.log("onfocusout "+value);
   if (currValue != value) {
-    console.log("FLASH");
+    return true;
+  }
+}
+
+function flash(n) {
+  if (compareValue(n)) {
+    var newN = getRandomIntInclusive(10, 80);
+    var newD = getRandomIntInclusive(newN, 100);
+    document.getElementById("numerator").innerText = newN;
+    document.getElementById("denominator").innerText = newD;
+    var newN2 = getRandomIntInclusive(40, 93);
+    var newD2 = getRandomIntInclusive(newN2, 95);
+    document.getElementById("numerator2").innerText = newN2;
+    document.getElementById("denominator2").innerText = newD2;
+    var flash1 = document.getElementsByClassName("flash")[0];
+    var flash2 = document.getElementsByClassName("flash")[1];
+    flash1.style.width = "68px";
+    flash2.style.width = "68px";
+    window.setTimeout(function() {
+      flash1.style.width = "0px";
+      flash2.style.width = "0px";
+    }, 1500);
   }
 }
 
